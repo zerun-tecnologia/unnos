eb1ef6a4d766ff1ebfc7ea68d3e842bfcf12aebc
Bruno de Araujo Alves <me@baraus.dev>
2025-03-19 16:02:35 -0300

    fix: add dimensions to chart

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -24,7 +24,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Button>nice</Button>
-        <Chart options={chartOptions} />
+        <Chart
+          options={chartOptions}
+          className="w-[400px] h-96"
+        />
         <Image
           className="dark:invert"
           src="/next.svg"
