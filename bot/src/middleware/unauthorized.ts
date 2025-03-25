b5134d270011f8b8fa5dc16df7c1342ea1cdb775
Bruno de Araujo Alves <me@baraus.dev>
2025-03-25 13:10:57 -0300

    chore: check middleware

--- bot/src/middleware/unauthorized.ts
+++ bot/src/middleware/unauthorized.ts
@@ -1,6 +1,6 @@
 import type { CacheType, ChatInputCommandInteraction } from 'discord.js'
 
-const bannedUsers = ['357015751057735682']
+const bannedUsers = ['357015751057735682', '765572736214761512']
 
 export function unauthorizedMiddleware(
   interaction: ChatInputCommandInteraction<CacheType>,
