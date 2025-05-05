50b0a3daa7cfbe2dba30c2565453343c9a24e534
Bruno de Araujo Alves <me@baraus.dev>
2025-05-05 13:07:50 -0300

    refactor: remove participants

--- prisma/migrations/20250430161727_add_editor/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Match" ADD COLUMN     "editorId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Match" ADD CONSTRAINT "Match_editorId_fkey" FOREIGN KEY ("editorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20250505160612_init_db/migration.sql
+++ prisma/migrations/20250505160612_init_db/migration.sql
@@ -15,20 +15,12 @@ CREATE TABLE "Guild" (
 );
 
 -- CreateTable
-CREATE TABLE "MatchBanned" (
-    "matchId" INTEGER NOT NULL,
-    "userId" TEXT NOT NULL,
-    "count" INTEGER NOT NULL,
-
-    CONSTRAINT "MatchBanned_pkey" PRIMARY KEY ("matchId","userId")
-);
-
--- CreateTable
 CREATE TABLE "Match" (
     "id" SERIAL NOT NULL,
     "name" TEXT,
     "guildId" TEXT NOT NULL,
     "winnerId" TEXT,
+    "editorId" TEXT,
     "status" TEXT NOT NULL DEFAULT 'open',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "finishedAt" TIMESTAMP(3),
@@ -45,11 +37,11 @@ CREATE TABLE "_user_guilds" (
 );
 
 -- CreateTable
-CREATE TABLE "_match_participants" (
+CREATE TABLE "_match_banned" (
     "A" INTEGER NOT NULL,
     "B" TEXT NOT NULL,
 
-    CONSTRAINT "_match_participants_AB_pkey" PRIMARY KEY ("A","B")
+    CONSTRAINT "_match_banned_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateTable
@@ -67,16 +59,13 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 CREATE INDEX "_user_guilds_B_index" ON "_user_guilds"("B");
 
 -- CreateIndex
-CREATE INDEX "_match_participants_B_index" ON "_match_participants"("B");
+CREATE INDEX "_match_banned_B_index" ON "_match_banned"("B");
 
 -- CreateIndex
 CREATE INDEX "_match_gave_B_index" ON "_match_gave"("B");
 
 -- AddForeignKey
-ALTER TABLE "MatchBanned" ADD CONSTRAINT "MatchBanned_matchId_fkey" FOREIGN KEY ("matchId") REFERENCES "Match"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "MatchBanned" ADD CONSTRAINT "MatchBanned_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Match" ADD CONSTRAINT "Match_editorId_fkey" FOREIGN KEY ("editorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Match" ADD CONSTRAINT "Match_guildId_fkey" FOREIGN KEY ("guildId") REFERENCES "Guild"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -91,10 +80,10 @@ ALTER TABLE "_user_guilds" ADD CONSTRAINT "_user_guilds_A_fkey" FOREIGN KEY ("A"
 ALTER TABLE "_user_guilds" ADD CONSTRAINT "_user_guilds_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_match_participants" ADD CONSTRAINT "_match_participants_A_fkey" FOREIGN KEY ("A") REFERENCES "Match"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_match_banned" ADD CONSTRAINT "_match_banned_A_fkey" FOREIGN KEY ("A") REFERENCES "Match"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "_match_participants" ADD CONSTRAINT "_match_participants_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "_match_banned" ADD CONSTRAINT "_match_banned_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_match_gave" ADD CONSTRAINT "_match_gave_A_fkey" FOREIGN KEY ("A") REFERENCES "Match"("id") ON DELETE CASCADE ON UPDATE CASCADE;
